0a0043d44cf1778e322cd9b49885630ebe10a9f9
"FareenaEhtesham" <"ehtesham4100390@cloud.neduet.edu.pk">
2022-06-25 16:55:39 +0500

    adding library inside a contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,55 +6,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "./AggregatorInterface.sol";
-// import directly from github
-//import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConvertor.sol";
 
 contract FundMe{
+
+    // call library
+    using PriceConvertor for uint256;
 
     uint256 public minimumUsd = 50;
     address payable public owner;
 
-    AggregatorV3Interface internal priceFeed;
-
     //structure of arrays
     address[] public funders;   // array of all the funders jo transactions[FUND] karaingy
+    /*SYNTAX
+    mapping(keyType => valueType).
+    */
     mapping(address => uint256) public fundersAmount;
-
-    constructor(){
-        // kovan network address
-        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
-    }
 
     function receiveFunds() public payable{
 
+        /*conversion(msg.value) === msg.value.conversion()
+        as conversion function defined in library contains an input
+
+        IF THIS FUNCTION EXPECT 2 INPUTS:
+        second parameter would be inside (....)
+        msg.value.conversion(123);
+        */
+
         // require is like a check if false so it print The minimum amount of........ 
-        require(conversion(msg.value) >= minimumUsd , "The minimum amount of fund would be >=50$");
+        require(msg.value.conversion() >= minimumUsd , "The minimum amount of fund would be >=50$");
         funders.push(msg.sender);   // msg.sender is the address of whoever calls the function
         fundersAmount[msg.sender] = msg.value;
         // Now we have to convert (msg.value) which may be in ether to USD
-    }
-
-    function getPrice() public view returns(uint256){
-        // Address
-        // Abi
-        (, int256 answer ,,,) = priceFeed.latestRoundData();
-        
-        //This function return ETH in terms of USD
-        return uint256(answer* 1e10); // 1**10
-    }
-
-    function getVersion() public view returns(uint256){
-
-        return priceFeed.version();
-    }
-
-    function conversion(uint256 ethAmount) public view returns(uint256){
-        // user send an ethereum and here in this function we convert it into dollars
-        uint256 ethPrice = getPrice(); // eth ki value in USD
-        uint256 ethinUSD = (ethAmount * ethPrice) / 1e18;
-        return ethinUSD;
-
     }
 
     function withdraw(uint256 amount) private{
---
+++ contracts/PriceConvertor.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./AggregatorInterface.sol";
+// import directly from github
+//import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConvertor{
+
+    function getPrice() internal view returns(uint256){
+        // Address
+        // Abi
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        (, int256 answer ,,,) = priceFeed.latestRoundData();
+        
+        //This function return ETH in terms of USD
+        return uint256(answer* 1e10); // 1**10
+    }
+ 
+    function getVersion() internal view returns(uint256){
+
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        return priceFeed.version();
+    }
+
+    function conversion(uint256 ethAmount) internal view returns(uint256){
+        // user send an ethereum and here in this function we convert it into dollars
+        uint256 ethPrice = getPrice(); // eth ki value in USD
+        uint256 ethinUSD = (ethAmount * ethPrice) / 1e18;
+        return ethinUSD;
+
+    }
+}
+
+/*
+As this library embed into contract, so all the functions inside a library must be internal
+
+*/
